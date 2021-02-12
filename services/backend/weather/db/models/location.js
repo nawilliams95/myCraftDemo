4954920c071d6746ad8405ad053cdb1394e1d589
Nykarri <karri.bonez@gmail.com>
2021-02-11 22:55:55 -0500

    Ran Migration
    generated a Locations model and migration file. edited them both with a more comfortable syntax based on sequelize documentation. ran successfullmigration and check database in console

---
+++ services/backend/weather/db/models/location.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+  const Location = sequelize.define('Location', {
+    cityName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    timeZone : {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    }, 
+    long: {
+      type: DataTypes.FLOAT,
+      allowNull: false
+    },
+    gridID: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }, 
+    gridX: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }, 
+    gridY: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+ });
+ return Location;
+};
